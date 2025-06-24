f69dae9998c404d19fe38680637ce8d5925a8951
2-Eung <eung4256@gmail.com>
2025-06-24 10:33:33 +0900

    CustomAnnotation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@interface CustomInfo {
+    String author();
+    String date();
+    int version() default 1;
+}
+
+@CustomInfo(author = "ㅇㅇㅎ", date = "2025-06-24", version = 2)
+class Demo {
+
+    @CustomInfo(author = "ㅇㅇㅎ", date ="2026-06-26")
+    public void display() {
+        System.out.println("Display method executed.");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+        Demo demo = new Demo();
+        Class<?> demoClass = demo.getClass();
 
+        if (demoClass.isAnnotationPresent(CustomInfo.class)) {
+            CustomInfo classInfo = demoClass.getAnnotation(CustomInfo.class);
+            System.out.println("Author: " + classInfo.author());
+            System.out.println("Date: " + classInfo.date());
+            System.out.println("Version: " + classInfo.version());
+        }
     }
 }
